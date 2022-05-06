60156ddfd174090e38b292b0b58603e713e6be1b
Sudheer Kumar Prajapat <sudheerkumar8929@gmail.com>
2022-05-06 23:35:35 +0530

    TakeLoan Component Almost Completed

--- frontend/src/Components/TakeLoan/TakeLoan.js
+++ frontend/src/Components/TakeLoan/TakeLoan.js
@@ -4,7 +4,16 @@ import style from './TakeLoan.module.css';
 
 const TakeLoan = () => {
   return (
-    <div>TakeLoan</div>
+    <div className={style.container}>
+      <h1 className={style.primaryHeading}>Take Loan</h1>
+      <form className={style.form}>
+        <h2 className={style.secondaryHeading}>Fill the below details carefully!</h2>
+        <input className={style.input} type="text" name="amount" id="amount" placeholder='Enter Loan Amount' required/>
+        <input className={style.input} type="text" name="tenure" id="tenure" placeholder='Enter Tenure (in months)' required/>
+        <input className={style.input} type="text" name="rate" id="rate" placeholder='Enter Interest Rate' required/>
+        <button className={style.btn} type='submit'>Submit</button>
+      </form>
+    </div>
   );
 }
 
