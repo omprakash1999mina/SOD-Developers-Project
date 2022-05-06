62eb0996a438c08dbe3c3a8ea438643e503514df
OP_Developer <op1999mina@gmail.com>
2022-05-07 00:14:42 +0530

    new features added

---
+++ frontend/firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDK8OAxXmdR0DQhjt1DdP_bHNl8AXw_xiU",
+    authDomain: "projectice-d44ad.firebaseapp.com",
+    projectId: "projectice-d44ad",
+    storageBucket: "projectice-d44ad.appspot.com",
+    messagingSenderId: "1080140713489",
+    appId: "1:1080140713489:web:5efcc78e44442a1591abb0",
+    measurementId: "G-T4M5YW8WRB"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+//   const analytics = getAnalytics(app);
