f43d065d79800824a81b14702b06905607d00ac1
Sudheer Kumar Prajapat <sudheerkumar8929@gmail.com>
2022-05-06 22:46:46 +0530

    Navbar Almost Completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,25 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
 import './App.css';
+import Navbar from './Components/Navbar/Navbar.js';
+import Home from './Components/Home/Home.js';
+import LoanRequests from './Components/LoanRequests/LoanRequests.js';
+import TakeLoan from './Components/TakeLoan/TakeLoan.js';
+import Profile from './Components/Profile/Profile.js';
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <>
+      <Router>
+        <Navbar></Navbar>
+        <Routes>
+          <Route exact path = '/' element={<Home/>}/>
+          <Route exact path = '/loanRequests' element={<LoanRequests/>}/>
+          <Route exact path = '/takeLoan' element={<TakeLoan/>}/>
+          <Route exact path = '/profile' element={<Profile/>}/>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
