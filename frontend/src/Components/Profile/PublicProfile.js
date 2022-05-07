695694b3201873618fd252d007161db2e6b40052
Sudheer Kumar Prajapat <sudheerkumar8929@gmail.com>
2022-05-07 13:40:19 +0530

    Public Profile Section Almost Completed

---
+++ frontend/src/Components/Profile/PublicProfile.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import style from './PublicProfile.module.css';
+
+const userData = {
+    profilePhoto : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7EAjufrsaffFdvLMDspiG0w_MG0N7eHUPUjz0bkF-v3qO7aFyyKxpKLA5lt7m0P2O_ZI&usqp=CAU',
+    name : 'Sudheer Kumar Prajapat',
+    age : '20',
+    email : 'example@gmail.com',
+    ctc : '20000'
+};
+
+const PublicProfile = () => {
+  return (
+    <>
+        <div className={style.container}>
+            <h1 className={style.heading}>Candidate Profile</h1>
+            <div className={style.profilePhoto}>
+                <img src={userData.profilePhoto} alt="Profile"/>
+            </div>
+            <div className={style.details}>
+                <p className={style.detail}>Name : {userData.name}</p>
+                <p className={style.detail}>Email : {userData.email}</p>
+                <p className={style.detail}>CTC : {userData.ctc}</p>
+                <p className={style.detail}>Age : {userData.age}</p>
+            </div>
+        </div>
+    </>
+  );
+}
+
+export default PublicProfile;
