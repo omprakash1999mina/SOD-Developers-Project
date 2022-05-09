214ec1115c2407f7b17a2aa4d31996a32b89b9c9
Devansh Gaur <89275109+Devansh-gaur-1611@users.noreply.github.com>
2022-05-09 12:33:49 +0530

    Privacy Policy And loan policy created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,8 @@ import Profile from './Components/Profile/Profile.js';
 import PublicProfile from './Components/Profile/PublicProfile.js';
 import CreateProfile from './Components/Profile/CreateProfile.js';
 import ModifyProfile from './Components/Profile/ModifyProfile.js';
-
+import PrivacyPolicy from "./Components/PrivacyPolicy/PrivacyPolicy";
+import LoanPolicy from "./Components/LoanPolicy/LoanPolicy"
 function App() {
   return (
     <>
@@ -23,6 +24,8 @@ function App() {
           <Route exact path = '/publicProfile' element={<PublicProfile/>}/>
           <Route exact path = '/createProfile' element={<CreateProfile/>}/>
           <Route exact path = '/modifyProfile' element={<ModifyProfile/>}/>
+          <Route exact path = '/privacypolicy' element={<PrivacyPolicy/>}/>
+          <Route exact path = '/loanpolicy' element={<LoanPolicy/>}/>
         </Routes>
       </Router>
     </>
