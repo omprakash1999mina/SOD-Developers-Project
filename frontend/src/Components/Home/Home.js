4ba162aa2c17afcc8366fe86b2e7be732ddb23ca
Devansh Gaur <89275109+Devansh-gaur-1611@users.noreply.github.com>
2022-05-09 11:14:06 +0530

    Home Page basic structure created

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
+import Card from "./card";
 
-import style from './Home.module.css';
-
+import style from "./Home.module.css";
+import giveLoan from "../../Assets/giveLoan.jpg";
+import takeLoan from "../../Assets/takeLoan.jpg";
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className={style.mainContainer}>
+      <div className={style.topContainer}>
+        <h1 className={style.title}>
+          Take Loan . Give Loan <br /> With No Fikrrr!!!!
+        </h1>
+      </div>
+      <div className={style.middleContainer}>
+        <h1 className={style.middleTitle}>Loan Section</h1>
+        <div className={style.middleMainContainer}>
+          <Card src={giveLoan} text="Want to give Loan" />
+          <Card src={takeLoan} text="Want to take Loan" />
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default Home;
