e35997164cd267e8df03059acc926f5001f7d17d
Sudheer Kumar Prajapat <sudheerkumar8929@gmail.com>
2022-05-09 19:31:24 +0530

    Comapany Name and Logo Updated

--- frontend/src/Components/PrivacyPolicy/PrivacyPolicy.js
+++ frontend/src/Components/PrivacyPolicy/PrivacyPolicy.js
@@ -31,8 +31,7 @@ const PrivacyPolicy = () => {
           </li>
           <li>
             Company (referred to as either "the Company", "We", "Us" or "Our" in
-            this Agreement) refers to VISHWESHWARNATH TECHNOLOGIES PRIVATE
-            LIMITED.
+            this Agreement) refers to LoanCircle.
           </li>
           <li>
             Device means any device that can access the Service such as a
@@ -59,11 +58,11 @@ const PrivacyPolicy = () => {
           <li>
             Usage Data refers to data collected automatically, either generated
             by the use of the Service or from the Service infrastructure itself
-            (for example, the duration of a page visit). */
+            (for example, the duration of a page visit).
           </li>
           <li>
-            Website refers to Dhandolat, accessible from
-            https://www.dhandolat.in
+            Website refers to LoanCircle, accessible from
+            https://www.loancircle.com
           </li>
           <li>
             You means the individual accessing or using the Service, or the
@@ -168,7 +167,7 @@ const PrivacyPolicy = () => {
           </li>
         </ol>
         <p>
-          Dhandolat is committed to safeguarding any information submitted to us
+          LoanCircle is committed to safeguarding any information submitted to us
           on this Website and/ or Services. The privacy notice provides you with
           a summary of the type of information collected by us, policy regarding
           use of that information and measures taken to safeguard that
@@ -224,7 +223,7 @@ const PrivacyPolicy = () => {
           us:
         </p>
         <ul className={styles.list}>
-          <li>By email: info@dhandolat.in </li>
+          <li>By email: info@loancircle.in </li>
         </ul>
       </div>
     </div>
