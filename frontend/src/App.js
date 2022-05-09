9b4c04eaf44cae03c968b1e10cf1e3d9ac2fbf03
Sudheer Kumar Prajapat <sudheerkumar8929@gmail.com>
2022-05-09 18:43:49 +0530

    Footer Almost Completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
-import './App.css';
+import styles from './App.module.css';
 import Navbar from './Components/Navbar/Navbar.js';
 import Home from './Components/Home/Home.js';
 import LoanRequests from './Components/LoanRequests/LoanRequests.js';
@@ -9,21 +9,25 @@ import Profile from './Components/Profile/Profile.js';
 import PublicProfile from './Components/Profile/PublicProfile.js';
 import CreateProfile from './Components/Profile/CreateProfile.js';
 import ModifyProfile from './Components/Profile/ModifyProfile.js';
+import Footer from "./Components/Footer/Footer.js";
 
 function App() {
   return (
     <>
       <Router>
-        <Navbar></Navbar>
-        <Routes>
-          <Route exact path = '/' element={<Home/>}/>
-          <Route exact path = '/loanRequests' element={<LoanRequests/>}/>
-          <Route exact path = '/takeLoan' element={<TakeLoan/>}/>
-          <Route exact path = '/profile' element={<Profile/>}/>
-          <Route exact path = '/publicProfile' element={<PublicProfile/>}/>
-          <Route exact path = '/createProfile' element={<CreateProfile/>}/>
-          <Route exact path = '/modifyProfile' element={<ModifyProfile/>}/>
-        </Routes>
+        <div className={styles.container}>
+          <Navbar></Navbar>
+          <Routes>
+            <Route exact path = '/' element={<Home/>}/>
+            <Route exact path = '/loanRequests' element={<LoanRequests/>}/>
+            <Route exact path = '/takeLoan' element={<TakeLoan/>}/>
+            <Route exact path = '/profile' element={<Profile/>}/>
+            <Route exact path = '/publicProfile' element={<PublicProfile/>}/>
+            <Route exact path = '/createProfile' element={<CreateProfile/>}/>
+            <Route exact path = '/modifyProfile' element={<ModifyProfile/>}/>
+          </Routes>
+          <Footer></Footer>
+        </div>
       </Router>
     </>
   );
