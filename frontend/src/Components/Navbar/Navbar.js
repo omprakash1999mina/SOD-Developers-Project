2435f8899a69040f3625d1d08b057eb83b9ee28a
Sudheer Kumar Prajapat <sudheerkumar8929@gmail.com>
2022-05-10 10:18:43 +0530

    Navbar & Home Updated for Small Size Screens

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -13,35 +13,39 @@ const Navbar = () => {
   const handelBurger = () => {
     setBurgerClicked(!burgerClicked);
   }
+  const handelBurgerByLogo = () => {
+    setBurgerClicked(false);
+  }
   const handelClick = () => {
     setIsClicked(!isClicked);
   }
   const handelLogout = () => {
     setIsLoggedIn(false);
     setIsProfileCreated(false);
+    handelBurger();
   }
 
   return (
     <>
       <nav className={`${style.navbar} ${(burgerClicked)?(style.expandNav):('')}`}>
+        <div className={`${style.logo} ${style.visible}`} onClick={handelBurgerByLogo}>
+          <Link className={style.link} to="/"><img src={logo} alt="Logo"/></Link>
+        </div>
         <ul className={`${style.navList} ${(!burgerClicked)?(style.hideVisibility):('')}`}>
-            <div className={style.logo}>
-              <Link className={style.link} to="/"><img src={logo} alt="Logo"/></Link>
-            </div>
-            <li><Link className={style.link} to="/">Home</Link></li>
-            <li><Link className={style.link} to="/loanRequests">Loan Requests</Link></li>
-            <li><Link className={style.link} to="/takeLoan">Take Loan</Link></li>
-            <li><Link className={style.link} to="/accountStatus">Account Status</Link></li>
+            <li><Link className={style.link} to="/" onClick={handelBurger}>Home</Link></li>
+            <li><Link className={style.link} to="/loanRequests" onClick={handelBurger}>Loan Requests</Link></li>
+            <li><Link className={style.link} to="/takeLoan" onClick={handelBurger}>Take Loan</Link></li>
+            <li><Link className={style.link} to="/accountStatus" onClick={handelBurger}>Account Status</Link></li>
         </ul>
         <div className={`${style.profile} ${(!burgerClicked)?(style.hideVisibility):('')}`} onClick={handelClick}>
           <img src={profile} alt="Profile"/>
           <div className={`${style.select} ${(!isClicked)?(style.hide):('')}`}>
-            {(isLoggedIn && isProfileCreated) && <Link className={style.selectLink} to='/profile'>Profile</Link>}
-            {((isLoggedIn) && isProfileCreated)?((isLoggedIn) && <Link className={style.selectLink} to='/modifyProfile'>Modify Profile</Link>):(
-               (isLoggedIn) && <Link className={style.selectLink} to='/createProfile'>Create Profile</Link>
+            {(isLoggedIn && isProfileCreated) && <Link className={style.selectLink} to='/profile' onClick={handelBurger}>Profile</Link>}
+            {((isLoggedIn) && isProfileCreated)?((isLoggedIn) && <Link className={style.selectLink} to='/modifyProfile' onClick={handelBurger}>Modify Profile</Link>):(
+               (isLoggedIn) && <Link className={style.selectLink} to='/createProfile' onClick={handelBurger}>Create Profile</Link>
             )}
             {(isLoggedIn)?(<Link className={style.selectLink} to='/' onClick={handelLogout}>Logout</Link>):(
-              <Link className={style.selectLink} to='/login'>Login</Link>
+              <Link className={style.selectLink} to='/login' onClick={handelBurger}>Login</Link>
             )}
           </div>
         </div>
