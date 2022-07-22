03119b9d5d685f6b50823102d887bc962844e92e
Sudheer Kumar Prajapat <99413988+sudheerkrp@users.noreply.github.com>
2022-07-22 09:22:19 +0530

    Fixed Title Issue

--- frontend/src/Components/Home/AboutUs.jsx
+++ frontend/src/Components/Home/AboutUs.jsx
@@ -11,11 +11,11 @@ const AboutUs = () => {
       </div>
       <div className={styles.paraContainerAbout}>
         <p className={styles.paraAbout}>
-          Loan circle is a fintech early-stage platform that works on a C2C
+          LoanCorner is a fintech early-stage platform that works on a C2C
           business model. In this, people can quickly get or give a loan to
           other individuals. Apart from that, individuals can ask other
           individuals for some modification in the loan plan. All the other
-          issues like loan security etc. will be managed by The Lone circle so
+          issues like loan security etc. will be managed by The LoneCorner so
           that the individual can give or get loans quickly without concerning
           the difficulties.
         </p>
