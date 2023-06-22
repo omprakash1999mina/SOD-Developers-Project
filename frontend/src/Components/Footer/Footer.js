46a9923bdc5ec25969aaa33b1c4606b48ffd0b7b
OP_Developer <op1999mina@gmail.com>
2023-06-22 13:20:43 +0530

    NewsLater updated

--- frontend/src/Components/Footer/Footer.js
+++ frontend/src/Components/Footer/Footer.js
@@ -39,7 +39,7 @@ const Footer = () => {
                 </div>
                 <div className="col-lg-3 col-md-6">
                     <h5 className="text-light mb-4">Newsletter</h5>
-                    <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
+                    <p>Stay informed, stay inspired. Our newsletter delivers finance updates directly to your inbox.</p>
                     <div className="position-relative mx-auto" style={{maxWidth: 400}}>
                         <input className="form-control bg-transparent w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email"/>
                         <button type="button" className="btn btn-secondary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
